299d6832d0043354e7503e35bfb975003ea67797
Rolands Kundzins <rolis.kungs@inbox.lv>
2025-03-12 09:06:34 +0200

    Get data in vue from flask API

--- flask-backend/api.py
+++ flask-backend/api.py
@@ -1,23 +1,22 @@
 import logging
 import os
 from datahub.ingestion.graph.client import DatahubClientConfig, DataHubGraph 
-# TODO: instead of using "pip install <dependency>" localy, figure out how to use the dependecies that docker has (.devcontainers)
-
 from flask import Flask
+from flask_cors import CORS
+
 
 app = Flask(__name__)
+CORS(app)
 log = logging.getLogger(__name__)
 logging.basicConfig(level=os.getenv("LOG_LEVEL") or "INFO")
-
 
 config = DatahubClientConfig(server="https://api.datahub.richert.li")
 graph = DataHubGraph(config)
 
+
 @app.route("/api")
 def get_data():
     log.info("Handling GET request for /api")
-
-    entity_list = []
 
     # If it is needed to process more than 10000 entities, then should use scrollAcrossEntities which seems to allow pagination
     query = """
@@ -42,6 +41,7 @@ def get_data():
 
     results = graph.execute_graphql(query)
     
+    entity_list = []
     for item in results.get("search", {}).get("searchResults", []):
         entity = item["entity"]
 
@@ -53,12 +53,11 @@ def get_data():
             "platform": entity["platform"]["name"].upper()
         })
 
-
-    '''
-    Use the DataHubGraph client to fetch all entities of type "dataset" from DataHub
-    https://datahubproject.io/docs/python-sdk/clients/#datahub.ingestion.graph.client.DataHubGraph
-    The required dependency is listed in the requirements.txt file
-    Use https://api.datahub.richert.li as the DataHub server (no authentication required) 
-    '''
-
     return entity_list
+
+'''
+Use the DataHubGraph client to fetch all entities of type "dataset" from DataHub
+https://datahubproject.io/docs/python-sdk/clients/#datahub.ingestion.graph.client.DataHubGraph
+The required dependency is listed in the requirements.txt file
+Use https://api.datahub.richert.li as the DataHub server (no authentication required) 
+'''
--- vue-frontend/src/App.vue
+++ vue-frontend/src/App.vue
@@ -9,6 +9,9 @@
         If you want to use the Swisscom style guide, you can find it here: https://sdx.swisscom.com/
 
         -->
+        <ul>
+            <li v-for="user in users" :key="user.id">{{ user.name }}</li>
+        </ul>
       </div>
     </div>
   </div>
@@ -16,8 +19,29 @@
 
 <script>
 import { defineComponent } from 'vue';
+import axios from 'axios';
+
+const API_BASE_URL = import.meta.env.FLASK_API_URL;
 
 export default defineComponent({
+    data() {
+        return {
+            responseData: null,
+            error: null
+        };
+    },
+    async mounted() {
+        console.log("I am mounted now");
+
+        try {
+            const response = await axios.get(`${API_BASE_URL}/api`);
+            this.message = response.data.message;
+
+            console.log("reponse.data:", response.data);
+        } catch (error) {
+            console.error("Error fetching data:", error);
+        }
+    }
   /*
 
   Fill this section with the necessary code to fetch the data from the server
